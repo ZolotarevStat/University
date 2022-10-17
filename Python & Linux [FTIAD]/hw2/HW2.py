3c92f2abee31dc9094110cecabb7d2cd26b02e48
Anton Zolotarev <90383935+ZolotarevStat@users.noreply.github.com>
2022-10-18 01:43:39 +0300

    Add files via upload

--- Python & Linux [FTIAD]/hw2/HW2.py
+++ Python & Linux [FTIAD]/hw2/HW2.py
@@ -2,85 +2,62 @@ import re
 
 
 class calculator():
-    def __init__(self, string: str = '1+1'):
+    def __init__(self, string='1+1'):
+        """
+        Инициализация всего на свете
+        :param string: входная строка
+        """
         self.string = string
-
-    def calculate(self):
-        def get_numbers(string):
-            # получаем чиселки
-            return [x for x in map(float, re.findall(r"(?:\d*\.\d+|\d+)", string))]
-
-        def get_order(string):
-            """
-            получаем порядок операций
-            :param string: исходная строка
-            :return:
-                symbols - все базовые математические операции и скобочки из строки
-                order - порядок операций
-            """
-            symbols = re.findall(r"[-+*/()]", string)
-            order = [x for x in symbols if x not in '()']
-            counter_order = 0
-            for i, s in enumerate(symbols):
-                if i + 1 != len(symbols):
-                    if s in '(':
-                        k = i + 1
-                        while symbols[k + 1] != ')':
-                            if symbols[k + 1] in '*/':
-                                order[counter_order] = symbols[k + 1]
-                                counter_order += 1
-                            elif symbols[k + 1] in '+-':
-                                order[counter_order] = symbols[k + 1]
-                                counter_order += 1
-                            k += 1
-                    elif s in '*/':
-                        order[counter_order] = symbols[i + 1]
-                        counter_order += 1
-                    elif symbols[i + 1] in '+-':
-                        order[counter_order] = symbols[i + 1]
-                        counter_order += 1
+        if self.string[0] in '+-':
+            self.string = '0' + self.string
+        self.operators = {'+': (1, lambda x, y: x + y), '-': (1, lambda x, y: x - y),
+                          '*': (2, lambda x, y: x * y), '/': (2, lambda x, y: x / y)}
+    def parse_and_calculate(self):
+        """
+            раскрываем исходную строку, переводим всё в польскую запись и считаем результат
+            :return: ответ
+        """
+        self.parsed_list = re.findall(r"(?:\d*\.\d+|\d+|[-+*/()])", self.string)
+        def get_polish():
+            stack = []
+            for token in self.parsed_list:
+                if token in self.operators:
+                    while stack and stack[-1] != "(" and self.operators[token][0] <= self.operators[stack[-1]][0]:
+                        yield stack.pop()
+                    stack.append(token)
+                elif token == ")":
+                    while stack:
+                        x = stack.pop()
+                        if x == "(":
+                            break
+                        yield x
+                elif token == "(":
+                    stack.append(token)
                 else:
-                    order[counter_order] = s
-            print(order)
-            print(symbols)
-            return order, symbols
+                    yield token
+            while stack:
+                yield stack.pop()
 
-        def __add__(current, other):
-            return current + other
+        stack = []
+        for token in get_polish():
+            if token in self.operators:
+                y, x = float(stack.pop()), float(stack.pop())
+                if token == '/':
+                    try:
+                        stack.append(self.operators[token][1](x, y))
+                    except ZeroDivisionError:
+                        return "Divide by 0 Error"
+                else:
+                    stack.append(self.operators[token][1](x, y))
+            else:
+                stack.append(token)
+        if int(stack[0]) == stack[0]:
+            return int(stack[0])
+        return stack[0]
 
-        def __sub__(current, other):
-            return current - other
+if __name__=='__main__':
+    #calculate = calculator("-5")
+    #calculate = calculator("100*2*3")
+    calculate = calculator("4-8*2+6*(2/3.123)")
+    print(calculate.get_generator())
 
-        def __mul__(current, other):
-            return current * other
-
-        def __truediv__(current, other):
-            try:
-                return current / other
-            except ZeroDivisionError:
-                return "Divide by 0 Error"
-
-        def __neg__(current):
-            # надо доработать или изменить
-            return -current
-
-        self.list = get_numbers(self.string)
-        self.order, self.symbols = get_order(self.string)
-        for i in range(len(self.list) - 1):
-            if self.order[i] == '+':
-                self.list[i + 1] = __add__(self.list[i], self.list[i + 1])
-            if self.order[i] == '-':
-                self.list[i + 1] = __sub__(self.list[i], self.list[i + 1])
-            if self.order[i] == '*':
-                self.list[i + 1] = __mul__(self.list[i], self.list[i + 1])
-            if self.order[i] == '/':
-                self.list[i + 1] = __truediv__(self.list[i], self.list[i + 1])
-                if isinstance(self.list[i + 1], str):
-                    return self.list[i + 1]
-        if int(self.list[-1]) == self.list[-1]:
-            return int(self.list[-1])
-        return self.list[-1]
-
-
-if __name__ == '__main__':
-    print(calculator("4-8*2+6*(2/3.123)").calculate())
